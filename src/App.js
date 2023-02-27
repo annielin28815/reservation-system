5602b0a123072693ca6b74a6495b6a1eb4a8e2d4
Annie <annielin28815@gmail.com>
2023-02-27 23:41:18 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
+      {/* <p className="text-3xl text-gray-700 font-bold mb-5">
         Welcome!
       </p>
       <p className="text-gray-500 text-lg">
         React and Tailwind CSS in action
-      </p>
+      </p> */}
+      <header className='grid grid-cols-12 gap-2 justify-between items-center'>
+        <div className='col-span-2'>Reseration</div>
+        <div className='col-span-8 mx-auto'>
+          <div className='flex bg-teal-700 text-green-50 px-8 py-4 rounded-full max-w-xl min-w-xs'>
+            <div>search</div>
+          </div>
+        </div>
+        <div className='col-span-2'>user</div>
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
