826d58867f4c8b7f95e4d6ead2274306348e0ba2
Annie <annielin28815@gmail.com>
2023-03-04 23:49:08 +0800

    custom font theme and define color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/
 
 const App = () => {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-4 m-10">
+    <div className="container mx-auto bg-gray-dark text-skin rounded-xl shadow p-4 m-10">
       {/* <p className="text-3xl text-gray-700 font-bold mb-5">
         Welcome!
       </p>
@@ -13,21 +13,23 @@ const App = () => {
         React and Tailwind CSS in action
       </p> */}
       <header className='grid grid-cols-12 gap-2 justify-between items-center'>
-        <div className='col-span-2'>Reseration</div>
+        <div className='col-span-2 tracking-widest text-2xl font-logo'>Reseration</div>
         <div className='col-span-8 mx-auto'>
-          <div className='border-2 border-teal-700 px-4 py-2 rounded-3xl h-10'>
-            {/* <div>search</div> */}
-            <FontAwesomeIcon icon={solid('magnifying-glass')} />
+          <div className='border border-teal-700 px-4 py-2 rounded-3xl h-10 grid grid-flow-col auto-cols-max justify-between items-center divide-x divide-white'>
+            <div className='px-4'>預約項目</div>
+            <div className='px-4'>月份</div>
+            <div className='px-4'>人數</div>
+            <div className='px-4'><FontAwesomeIcon icon={solid('magnifying-glass')} color="#c1b2ab" /></div>
           </div>
         </div>
         <div className='col-span-2'>
           <div className='grid grid-cols-2 gap-4 justify-between items-center'>
             <div className=''>
-              <div className='bg-teal-700 text-green-50 px-2 py-2 rounded-full h-10 text-center'>
+              <div className='bg-skin text-gray-dark px-2 py-2 rounded-full h-10 text-center'>
                 直接預約
               </div>
             </div>
-            <div className=' bg-gray-100 text-teal-700 rounded-full font-bold text-center w-10 h-10 p-2'>
+            <div className='border-2 border-skin rounded-full font-bold text-center w-10 h-10 p-2'>
               A
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,48 @@ module.exports = {
     "./src/**/*.{js, jsx, ts, tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    fontFamily: {
+      'sans': ['Baloo 2'],
+      'logo': ['Courgette'],
+      'sans-tc': ['Noto Sans TC']
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'pink': '#f6d3da',
+      'gray-dark': '#3e3d40',
+      'gray': '#8492a6',
+      'gray-light': '#87888a',
+      'skin': '#c1b2ab',
+      'brown': '#a29078'
+    },
   },
   plugins: ['macros'],
+  "files.associations": {
+    "*.css": "tailwindcss"
+  },
+  "editor.quickSuggestions": {
+    "strings": true
+  },
+  "tailwindCSS.includeLanguages": {
+    "plaintext": "html"
+  }
 }
