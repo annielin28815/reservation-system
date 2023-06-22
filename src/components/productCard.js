66097092c5eff8ceacc222ed9c92383760b671c5
Annie <annielin28815@gmail.com>
2023-06-22 23:56:35 +0800

    replace images from assets to openapi source

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -5,7 +5,7 @@ import '../index.css'
 const ProductCard = (props) => {
 
   return <div className="col-span-3 w-80">
-    <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+    <img src="https://fakeimg.pl/320x160" alt="product-img" />
   </div>
 }
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,5 @@
 import React from "react";
-import '../index.css'
-import  product01  from '../assets/images/product01.jpg';
-import  product04  from '../assets/images/product04.jpg';
-import  product08  from '../assets/images/product08.jpg';
-import  product07  from '../assets/images/product07.jpg';
+import '../index.css';
 import ServiceCard from '../components/serviceCard';
 import ProductCard from '../components/productCard';
 
@@ -13,48 +9,48 @@ const HomePage = (props) => {
       id: 1,
       title: '美甲服務',
       des: '閒坐悲君亦自悲，萬國衣冠拜冕旒，禁裡疏鐘官舍晚，雙袖龍鍾淚不乾，孤帆遠影碧空盡，望帝春心託杜鵑，曾是寂寥金燼暗，卻話巴山夜雨時，為有雲屏無限嬌，九重誰省諫書函，無邊落木蕭蕭下，日暮聊為梁父吟，煙籠寒水月籠沙，楚腰纖細掌中輕，繁華事散逐香塵，野渡無人舟自橫，關門令尹誰能識，何如此處學長生，別夢依依到謝家，剔開紅燄救飛蛾，日光斜照集靈臺，兩三星火是瓜洲，冰簟銀床夢不成，北斗闌干南斗斜，寂寞空庭春欲晚，顧影無如白髮何，千尋鐵鎖沈江底，袍褲宮人掃御床。',
-      pic: product01
+      pic: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 2,
       title: '眉型設計',
       des: '閒坐悲君亦自悲，萬國衣冠拜冕旒，禁裡疏鐘官舍晚，雙袖龍鍾淚不乾，孤帆遠影碧空盡，望帝春心託杜鵑，曾是寂寥金燼暗，卻話巴山夜雨時，為有雲屏無限嬌，九重誰省諫書函，無邊落木蕭蕭下，日暮聊為梁父吟，煙籠寒水月籠沙，楚腰纖細掌中輕，繁華事散逐香塵，野渡無人舟自橫，關門令尹誰能識，何如此處學長生，別夢依依到謝家，剔開紅燄救飛蛾，日光斜照集靈臺，兩三星火是瓜洲，冰簟銀床夢不成，北斗闌干南斗斜，寂寞空庭春欲晚，顧影無如白髮何，千尋鐵鎖沈江底，袍褲宮人掃御床。',
-      pic: product04
+      pic: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 3,
       title: '日式嫁接',
       des: '閒坐悲君亦自悲，萬國衣冠拜冕旒，禁裡疏鐘官舍晚，雙袖龍鍾淚不乾，孤帆遠影碧空盡，望帝春心託杜鵑，曾是寂寥金燼暗，卻話巴山夜雨時，為有雲屏無限嬌，九重誰省諫書函，無邊落木蕭蕭下，日暮聊為梁父吟，煙籠寒水月籠沙，楚腰纖細掌中輕，繁華事散逐香塵，野渡無人舟自橫，關門令尹誰能識，何如此處學長生，別夢依依到謝家，剔開紅燄救飛蛾，日光斜照集靈臺，兩三星火是瓜洲，冰簟銀床夢不成，北斗闌干南斗斜，寂寞空庭春欲晚，顧影無如白髮何，千尋鐵鎖沈江底，袍褲宮人掃御床。',
-      pic: product08
+      pic: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 4,
       title: '半永久霧眉',
       des: '閒坐悲君亦自悲，萬國衣冠拜冕旒，禁裡疏鐘官舍晚，雙袖龍鍾淚不乾，孤帆遠影碧空盡，望帝春心託杜鵑，曾是寂寥金燼暗，卻話巴山夜雨時，為有雲屏無限嬌，九重誰省諫書函，無邊落木蕭蕭下，日暮聊為梁父吟，煙籠寒水月籠沙，楚腰纖細掌中輕，繁華事散逐香塵，野渡無人舟自橫，關門令尹誰能識，何如此處學長生，別夢依依到謝家，剔開紅燄救飛蛾，日光斜照集靈臺，兩三星火是瓜洲，冰簟銀床夢不成，北斗闌干南斗斜，寂寞空庭春欲晚，顧影無如白髮何，千尋鐵鎖沈江底，袍褲宮人掃御床。',
-      pic: product07
+      pic: 'https://fakeimg.pl/320x160/'
     },
   ];
 
   const productList = [
     {
       id: 1,
-      img: 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+      img: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 2,
-      img: 'https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+      img: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 3,
-      img: 'https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+      img: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 4,
-      img: 'https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+      img: 'https://fakeimg.pl/320x160/'
     },
     {
       id: 5,
-      img: 'https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
+      img: 'https://fakeimg.pl/320x160/'
     },
   ];
 
